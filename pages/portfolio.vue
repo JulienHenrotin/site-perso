f1132425bde5aa4bd8597a568dc8f01a67ca00ea
Julien <julien@henoo.fr>
2025-05-11 22:44:27 +0200

    intégration lottie

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,9 +1,15 @@
 <!-- pages/index.vue -->
 <template>
-    <div class="h-screen w-screen flex items-center justify-center bg-gray-100">
-
+    <div class="h-screen w-screen flex items-center justify-center ">
 
         en construction
 
+        <DotLottieVue style="height: 500px; width: 500px" autoplay loop
+            src="https://lottie.host/8c657af1-c1c5-4027-b514-d1baf9a48106/facqh9VWLh.lottie" />
+
     </div>
 </template>
+
+<script lang="ts">
+
+</script>
---
+++ plugins/dotlottie.client.ts
@@ -0,0 +1,6 @@
+import { defineNuxtPlugin } from '#app'
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.component('DotLottieVue', DotLottieVue)
+})
