fc32b74c3ee2db03e05110126ea53996795acae8
Julien <julien@henoo.fr>
2025-05-25 13:10:18 +0200

    Test couleur et description

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center py-12 space-y-12">
+  <div
+    class="min-h-screen flex flex-col items-center justify-center py-12 space-y-12 bg-gradient-to-b from-slate-900 to-slate-800">
     <!-- ───────────────────────── Top row : Avatar + Description ───────────────────────── -->
     <div class="flex flex-col md:flex-row items-center justify-center md:space-x-10 space-y-6 md:space-y-0">
       <!-- Avatar + Mascotte -->
       <div class="relative inline-block group">
         <img src="~/assets/images/moi.jpg" alt="Photo de profil"
-          class="rounded-full object-cover size-24 sm:size-48 z-10 relative">
+          class="rounded-full object-cover size-24 sm:size-48 z-10 relative border-4 border-emerald-500">
         <img src="~/assets/images/juju/hello.png" alt="Mascotte" class="absolute left-0 top-1/2 w-20 sm:w-28
                  -translate-y-1/2 opacity-0
                  transition-all duration-500
@@ -15,18 +16,15 @@
                  group-hover:-rotate-12">
       </div>
 
-
-
       <!-- Description -->
       <div class="max-w-xl text-center space-y-4">
         <h1 class="text-4xl font-bold text-primary-600">Julien Henrotin</h1>
         <p class="text-lg leading-relaxed">
-          Développeur mobile Flutter et full-stack JavaScript—passionné
-          par l’UX, la performance et l’open-source.
+          Développeur mobile Flutter et front-end web (Nuxt, Vue.js, Tailwind CSS),
+          curieux et agile pour apprendre rapidement de nouvelles technologies comme React ou Java.
         </p>
         <p class="text-gray-400">
-          Toujours curieux d’apprendre de nouvelles technologies et de relever
-          des défis complexes !
+          Toujours motivé par l’envie de créer des interfaces soignées, performantes et centrées utilisateur.
         </p>
       </div>
     </div>
