2e32259584c4080b49a418169d33ffe20e8dd354
Julien <julien@henoo.fr>
2025-05-27 10:54:04 +0200

    i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/image', 'nuxt-rive'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/image', 'nuxt-rive', '@nuxtjs/i18n',],
   nitro: {
     preset: 'static'
   },
@@ -10,6 +10,30 @@ export default defineNuxtConfig({
   ui: {
     theme: {
       colors: ['primary', 'secondary', 'tertiary', 'info', 'success', 'warning', 'error', 'neutral']
+    }
+  },
+
+  experimental: {
+    typedPages: true
+  },
+
+
+  i18n: {
+    defaultLocale: 'fr',
+    strategy: 'prefix_except_default',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json', language: 'en-US' },
+      { code: 'fr', name: 'Français', file: 'fr.json', language: 'fr-FR' }
+    ],
+
+    lazy: true,
+
+    langDir: 'locales',
+
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
     }
   }
 })
